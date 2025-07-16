b21aa74b9d639eef460bebab7228ae0475a1787c
Devin <devinrsantillanes@gmail.com>
2025-07-16 07:51:59 -0600

    In process of completing activity list and single activity page

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,6 +1,6 @@
 import { useAuth } from "../auth/AuthContext";
 import useQuery from "../api/useQuery";
-import useMutation from "../api/useMutation";
+// import useMutation from "../api/useMutation";
 
 /** Shows a list of activities. */
 export default function ActivityList() {
@@ -10,8 +10,8 @@ export default function ActivityList() {
     error,
   } = useQuery("/activities", "activities");
 
-  if (loading || !activities) return <p>Loading...</p>;
-  if (error) return <p>Sorry! {error}</p>;
+  // if (loading || !activities) return <p>Loading...</p>;
+  // if (error) return <p>Sorry! {error}</p>;
 
   return (
     <ul>
@@ -19,26 +19,5 @@ export default function ActivityList() {
         <ActivityListItem key={activity.id} activity={activity} />
       ))}
     </ul>
-  );
-}
-
-/** Shows a single activity. Logged-in users will also see a delete button. */
-function ActivityListItem({ activity }) {
-  const { token } = useAuth();
-  const {
-    mutate: deleteActivity,
-    loading,
-    error,
-  } = useMutation("DELETE", "/activities/" + activity.id, ["activities"]);
-
-  return (
-    <li>
-      <p>{activity.name}</p>
-      {token && (
-        <button onClick={() => deleteActivity()}>
-          {loading ? "Deleting" : error ? error : "Delete"}
-        </button>
-      )}
-    </li>
   );
 }
